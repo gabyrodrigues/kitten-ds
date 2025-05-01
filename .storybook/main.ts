532de505bb7380a6eee0ca74b514aa7a9b07de11
gabyrodrigues <gabyrlima.99@gmail.com>
2025-04-30 21:09:33 -0300

    :wrench: config storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
 		"@storybook/addon-essentials",
 		"@chromatic-com/storybook",
 		"@storybook/experimental-addon-test",
+		"@storybook/addon-a11y",
 	],
 	framework: {
 		name: "@storybook/react-vite",
