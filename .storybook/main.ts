7d129e81be03c66d727f3198a46fc325348be78a
gabyrodrigues <gabyrlima.99@gmail.com>
2025-05-16 22:02:03 -0300

    :wrench: Update Storybook bg colors

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,17 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
-	stories: [
-		"../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-		"../src/stories/docs/**/*.mdx",
-	],
-	addons: [
-		"@storybook/addon-essentials",
-		"@chromatic-com/storybook",
-		"@storybook/experimental-addon-test",
-		"@storybook/addon-a11y",
-	],
-	framework: {
-		name: "@storybook/react-vite",
-		options: {},
-	},
-	docs: {},
-};
-export default config;
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/stories/docs/**/*.mdx"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-a11y"
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  docs: {}
+}
+export default config
