28870f9f451a357058d3f4d0aa3bf5ded39f5af1
gabyrodrigues <gabyrlima.99@gmail.com>
2025-06-11 21:34:31 -0300

    :wrench: storybook theme config + new storybook logo

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,11 @@
+import { create } from "@storybook/theming"
+
+export default create({
+  fontBase: '"Inter", sans-serif',
+  fontCode: "monospace",
+  base: "dark",
+  brandTitle: "Kitten DS",
+  brandUrl: "#", //TODO: Update project's URL after publishing
+  brandImage: "/logo.svg",
+  brandTarget: "_self"
+})
