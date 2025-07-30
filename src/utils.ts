560697a7f3a556e65019620aa41696cecbbff3a2
gabyrodrigues <gabyrlima.99@gmail.com>
2025-07-30 17:12:05 -0300

    :recycle: Refactor tailwind variants merge configs + fix borders

--- src/utils.ts
+++ src/utils.ts
@@ -1,68 +1,71 @@
 import { type ClassValue, clsx } from "clsx"
 import { type DefaultClassGroupIds, extendTailwindMerge } from "tailwind-merge"
+import { type TV, tv as tvBase } from "tailwind-variants"
 
-const borderWidthTokens = [
-  "border-thin",
-  "border-medium",
-  "border-thick",
-  "border-bold",
-  "border-heavy"
-]
+const borderWidthTokens = ["thin", "medium", "thick", "bold", "heavy"]
+
+const twMergeConfig = {
+  classGroups: {
+    "font-size": [
+      "text-display1",
+      "text-display2",
+      "text-display3",
+      "text-h1",
+      "text-h2",
+      "text-h3",
+      "text-h4",
+      "text-h5",
+      "text-h6",
+      "text-body1",
+      "text-body2",
+      "text-body3",
+      "text-body4",
+      "text-body5",
+      "text-button",
+      "text-chip",
+      "text-label"
+    ],
+    ...Object.fromEntries(
+      (
+        [
+          ["border-w", "border"],
+          ["border-w-x", "border-x"],
+          ["border-w-y", "border-y"],
+          ["border-w-t", "border-t"],
+          ["border-w-r", "border-r"],
+          ["border-w-b", "border-b"],
+          ["border-w-l", "border-l"],
+          ["border-w-s", "border-s"],
+          ["border-w-e", "border-e"]
+        ] satisfies Array<[DefaultClassGroupIds, string]>
+      ).map(([key, value]) => [key, { [value]: borderWidthTokens }])
+    )
+  }
+}
 
 export const CUSTOM_TW_MERGE = extendTailwindMerge({
   extend: {
     classGroups: {
-      "font-size": [
-        "text-display1",
-        "text-display2",
-        "text-display3",
-        "text-h1",
-        "text-h2",
-        "text-h3",
-        "text-h4",
-        "text-h5",
-        "text-h6",
-        "text-body1",
-        "text-body2",
-        "text-body3",
-        "text-body4",
-        "text-body5",
-        "text-button",
-        "text-chip",
-        "text-label"
-      ],
-      // "border-w": [...borderWidthTokens]
-      // "border-w": [{ border: borderWidthTokens }],
-      // "border-w-x": [{ "border-x": borderWidthTokens }],
-      // "border-w-y": [{ "border-y": borderWidthTokens }],
-      // "border-w-t": [{ "border-t": borderWidthTokens }],
-      // "border-w-r": [{ "border-r": borderWidthTokens }],
-      // "border-w-b": [{ "border-b": borderWidthTokens }],
-      // "border-w-l": [{ "border-l": borderWidthTokens }],
-      // "border-w-s": [{ "border-s": borderWidthTokens }],
-      // "border-w-e": [{ "border-e": borderWidthTokens }]
-      ...Object.fromEntries(
-        // https://github.com/dcastil/tailwind-merge/discussions/393#discussioncomment-12287055
-        (
-          [
-            ["border-w", "border"],
-            ["border-w-x", "border-x"],
-            ["border-w-y", "border-y"],
-            ["border-w-t", "border-t"],
-            ["border-w-r", "border-r"],
-            ["border-w-b", "border-b"],
-            ["border-w-l", "border-l"],
-            ["border-w-s", "border-s"],
-            ["border-w-e", "border-e"]
-          ] satisfies Array<[DefaultClassGroupIds, string]>
-        ).map(([key, value]) => [key, { [value]: borderWidthTokens }])
-      )
+      ...twMergeConfig.classGroups
     }
   },
   override: {
     conflictingClassGroups: {}
   }
 })
+
+export const tv: TV = (options, config) =>
+  tvBase(options, {
+    ...config,
+    twMerge: config?.twMerge ?? true,
+    twMergeConfig: {
+      ...config?.twMergeConfig,
+      classGroups: {
+        ...config?.twMergeConfig?.classGroups,
+        ...twMergeConfig.classGroups
+      }
+    }
+  })
 
 export function cn(...inputs: ClassValue[]) {
   return CUSTOM_TW_MERGE(clsx(inputs))
